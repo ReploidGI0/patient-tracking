5b096b08b5e6e419d927da510bd1814a57d9976e
ReploidGI0 <gilespericueta@gmail.com>
2025-05-07 13:24:23 -0700

    Funcion updatePatient implementada

--- src/store.ts
+++ src/store.ts
@@ -9,6 +9,7 @@ type PatientState = {
     addPatient: (data: DraftPatient) => void
     deletePatient: (id: Patient['id']) => void
     getPatientById: (id: Patient['id']) => void
+    updatePatient: (data: DraftPatient) => void
 }
 
 const createPatient = (patient: DraftPatient) : Patient => { //Toma un paciente draft y retorna un paciente (con ID)
@@ -34,6 +35,13 @@ export const usePatientStore = create<PatientState>()(
         getPatientById: (id) => {
             set(() => ({
                 activeId: id
+            }))
+        },
+        updatePatient: (data) => {
+            set((state) => ({
+                patients: state.patients.map(patient => patient.id === state.activeId ? {id: state.activeId, 
+                    ...data } : patient),
+                    activeId: ''
             }))
         }
     })

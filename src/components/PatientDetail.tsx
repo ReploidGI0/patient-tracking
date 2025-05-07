4646dda593d63f398efe5a37eaeaff8a914e3fdb
ReploidGI0 <gilespericueta@gmail.com>
2025-05-06 23:24:22 -0700

    PatientDetail y PatientDetailItem con estilos completados

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,4 +1,5 @@
 import type { Patient } from '../types'
+import PatientDetailItem from './PatientDetailItem'
 
 type PatientDetailProps = {
     patient: Patient
@@ -6,8 +7,27 @@ type PatientDetailProps = {
 
 export default function PatientDetail({patient}: PatientDetailProps) {
   return (
-    <div>
-        {patient.name}
+    <div className='mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl'>
+        <PatientDetailItem label='ID' data={patient.id}/>
+        <PatientDetailItem label='Nombre' data={patient.name}/>
+        <PatientDetailItem label='Propietario' data={patient.caretaker}/>
+        <PatientDetailItem label='Email' data={patient.email}/>
+        <PatientDetailItem label='Fecha Alta' data={patient.date.toString()}/>
+        <PatientDetailItem label='Sintomas' data={patient.symptoms}/>
+
+        <div className=' flex justify-between mt-10'>
+            <button
+                className='py-2 px-10 bg-indigo-600 hover:bg-indigo-800 
+                text-white font-bold uppercase rounded-lg'
+            >Editar</button>
+
+            <button
+                className='py-2 px-10 bg-red-600 hover:bg-red-800 
+                text-white font-bold uppercase rounded-lg'
+            >Eliminar</button>
+        </div>
     </div>
+
+
   )
 }
